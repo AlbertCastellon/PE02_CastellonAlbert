40cfc476709d1d4099060a29093d777fde8a53e3
Pepe Sufragio <albertkstyon@gmail.com>
2025-10-02 17:53:14 +0200

    main code done

---
+++ PE02_CastellonAlbert.java
@@ -0,0 +1,158 @@
+import java.util.Scanner;
+
+public class PE02_CastellonAlbert {
+    public static void main(String[] args) {
+        Scanner escaner = new Scanner(System.in);
+        int or = 50;
+        String weapon, weaponCode, direction, spell;
+        weapon = "res";
+        weaponCode = "res";
+        spell = "cap";
+        boolean mirarAltresParades, comprarPocio, potion, comprarPenjoll, necklace, knockDoor, alive;
+        alive = true;
+        necklace = false;
+        potion = false;
+        System.out.println("Vas al mercat a comprar una arma, has d'explorar el bosc i pot ser perillós així podràs protegir-te.");
+        System.out.println("Tens " + or + " or per gastar.");
+        System.out.println("Vols mirar altres parades del mercat? (true/false)");
+        mirarAltresParades = escaner.nextBoolean();
+        if (mirarAltresParades) {
+            System.out.println("Pases per una parada que venen pocions per 10 or.");
+            System.out.println("Vols comprar una poció? (true/false)");
+            comprarPocio = escaner.nextBoolean();
+            if (comprarPocio) {
+                or -= 10;
+                System.out.println("Has comprat una poció. Et queden " + or + " or.");
+                potion = true;
+            } else {
+                System.out.println("No has comprat res");
+            }
+            System.out.println("Veus un penjoll amb forma de lluna que et crida l'atenció.");
+            System.out.println("Vols comprar el penjoll per 10 or? (true/false)");
+            comprarPenjoll = escaner.nextBoolean();
+            if (comprarPenjoll) {
+                or -= 10;
+                System.out.println("Has comprat el penjoll. Et queden " + or + " or.");
+                necklace = true;
+            } else {
+                System.out.println("No has comprat res");
+            }
+        }
+        System.out.println("Arribes a la parada d'armes. Tens " + or + " or per gastar.");
+        System.out.println("Mires les armes disponibles.");
+        System.out.println("1. Espasa llarga per 40 or (LS)");
+        System.out.println("2. Espasa curta per 35 or (SS)");
+        System.out.println("3. Daga per 20 or (D)");
+        System.out.println("Quina arma vols comprar? (LS/SS/D)");
+        
+        weaponCode = escaner.next();
+        switch (weaponCode) {
+            case "LS":
+                if (or >= 40) {
+                    or -= 40;
+                    weapon = "Espasa llarga";
+                    System.out.println("Has comprat una " + weapon + ". Et queden " + or + " or.");
+                } else {
+                    System.out.println("No tens prou or per comprar aquesta arma.");
+                    weaponCode = "res";
+                }
+                break;
+            case "SS":
+                if (or >= 35) {
+                    or -= 35;
+                    weapon = "Espasa curta";
+                    System.out.println("Has comprat una " + weapon + ". Et queden " + or + " or.");
+                    if (necklace) {
+                        System.out.println("El venedor et regala un escut.");
+                        weapon = "Espasa curta i escut";
+                        weaponCode = "SSS";
+                    }
+                } else {
+                    System.out.println("No tens prou or per comprar aquesta arma.");
+                    weaponCode = "res";
+
+                }
+                break;
+            case "D":
+                if (or >= 20) {
+                    or -= 20;
+                    weapon = "Daga";
+                    System.out.println("Has comprat una " + weapon + ". Et queden " + or + " or.");
+                    if (necklace) {
+                        System.out.println("El venedor et regala una altra daga.");
+                        weapon = "2 dagues";
+                        weaponCode = "DD";
+                    }
+                } else {
+                    System.out.println("No tens prou or per comprar aquesta arma.");;
+                }
+                break;
+            default:
+                System.out.println("No has seleccionat una arma vàlida. El venedor et fa marxar per fer-li perdre el temps.");
+                break;
+        }
+        System.out.println("Armat amb " + weapon + ", et dirigeixes cap al bosc.");
+        if (potion) {
+            System.out.println("Tens una poció per curar-te si et fan mal.");
+        }
+        System.out.println("Surts del poble i t'endinses al bosc. Què vols explorar, el nord o l'est?");
+        direction = escaner.next();
+        if (!direction.equals("nord") && !direction.equals("est")) {
+            System.out.println("T'has perdut i no saps tornar al poble. Acabes morint de gana.");
+        } else if (direction.equals("nord")) {
+            System.out.println("Mentre explores el bosc cap al nord, un llop t'ataca.");
+            if(weaponCode.equals("SSS")) {
+                System.out.println("Gràcies a l'escut que t'ha regalat el venedor, aconsegueixes matar el llop sense patir cap ferida.");
+            }else if (weaponCode.equals("D")) {
+                System.out.println("Només amb una daga mates el llop però acabes greument ferit.");
+                if (potion) {
+                    System.out.println("Per sort, tens una poció i aconsegueixes curar-te.");
+                } else {
+                    System.out.println("Com no tens cap poció, acabes morint per les ferides.");
+                    alive = false;
+                }
+            }else if (weaponCode.equals("res")) {
+                System.out.println("Com estas desarmat, el llop et destrossa i acabes morint.");
+                alive = false;
+            } else {
+                System.out.println("Ets ferit lleument però aconsegueixes matar el llop.");
+                System.out.println("Envenes la ferida amb la roba que portes. I atures el sagnat.");
+            }
+            if(alive) {
+                System.out.println("Explores la zona i tornes al poble sa i estalvi.");
+            }
+        } else {
+            System.out.println("Explorant el bosc cap a l'est, trobes una cabana.");
+            System.err.println("Vols entrar sense trucar? (true/false)");
+            knockDoor = escaner.nextBoolean();
+            if (knockDoor) {  
+                System.out.println("Al obrir la porta t'encega una llum i notes com et fas petit. El mag que viu a la cabana t'ha convertit en un gripau." );
+            } else {
+                System.out.println("Truques a la porta.");
+                System.out.println("Un home d'avançada edat t'obre la porta i et convida a entrar.");
+                if (!necklace) {
+                    System.out.println("L'home et serveix te i parleu una estona. Abans de marxar et comenta que sempre li ha agradat viure en tranquilitat però que des de que va morir la seva dona, se sent molt sol. Recorda a la seva dona i comenta que sempre portava un penjoll amb forma de lluna.");
+                    System.out.println("Has explorat la part est del bosc i tornes al poble sa i estalvi.");
+                } else {
+                    System.out.println("L'home veu el penjoll que portes i et revela que es un mag i que et pot ensenyar un encanteri.");
+                    if (weaponCode.equals("DD")) {
+                        System.out.println("Com portes dues dagues, el mag t'ensenya l'encanteri per fer-te invisible.");
+                        spell = "invisibilitat";
+                    } else if (weaponCode.equals("SSS")) {
+                            System.out.println("Com portes una espasa curta i un escut, el mag t'ensenya l'encanteri per fer-te invulnerable.");
+                            spell = "invulnerabilitat";
+                    } else if (weaponCode.equals("LS")) {
+                            System.out.println("Com portes una espasa llarga, el mag t'ensenya l'encanteri per augmentar la teva força.");
+                            spell = "superforça";
+                    } else {
+                            System.out.println("Com no portes cap arma, el mag t'ensenya l'encanteri per crear una arma de llum.");
+                            spell = "arma de llum";
+                        }
+                    System.out.println("Has explorat la part est del bosc i tornes al poble sa i estalvi.");
+                    System.out.println("Ara saps l'encanteri " + spell + ".");
+                }
+            }
+        }
+        escaner.close();
+    }
+}
